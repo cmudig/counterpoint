01c08849187f5a492a706ba5236af80d22b7100b
Venkatesh Sivaraman <venkatesh.sivaraman.98@gmail.com>
2023-10-04 14:41:46 -0400

    Project scaffolding

---
+++ lib/main.ts
@@ -0,0 +1,3 @@
+export function helloWorld(a: number, b: number): number {
+  return a + b;
+}
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+
+module.exports = defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'lib/main.ts'),
+      name: 'canvas-animation',
+      fileName: (format) => `canvas-animation.${format}.js`,
+    },
+  },
+  plugins: [dts()],
+});
